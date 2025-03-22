99437f5541601dff67fe6fc18b3212c28917db1e
puppersalt33 <snowhuntet@gmail.com>
2025-03-22 18:15:53 +0300

    Rebuild maven setup for clone GIT

--- src/main/java/org/example/atm_maven_jfx/Main.java
+++ src/main/java/org/example/atm_maven_jfx/Main.java
@@ -15,9 +15,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Scene previousScene = new Scene(new javafx.scene.layout.StackPane(), 400, 300);
-
         FirstAuth firstAuth = new FirstAuth(previousScene);
         firstAuth.start(primaryStage);
+        primaryStage.setTitle("ATM Application"); // Добавлен заголовок для удобства
         primaryStage.show();
     }
 }
